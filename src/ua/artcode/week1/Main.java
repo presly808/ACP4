2343e458e37b4ba72504034e967e81b45c83f7f8
presly808 <presly808@gmail.com>
2015-01-18 13:46:05 +0200

    week1 examples fix

---
+++ src/ua/artcode/week1/Main.java
@@ -0,0 +1,20 @@
+package ua.artcode.week1;
+
+/**
+ * Created by serhii on 17.01.15.
+ */
+public class Main {
+
+    //Integer - int, Double, Character
+    public static void main(String[] args) {
+
+        String some = "go";
+        for (int i = 0; i < 100; i++) {
+            some += i;
+        }
+
+
+
+    }
+
+}
